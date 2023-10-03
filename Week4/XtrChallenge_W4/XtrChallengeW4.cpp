7e3fbd076eabc44caad0d54f2a571d1b8e4e5b00
Nikolas Kazanjian <nikolas.kazanjiann@gmail.com>
2023-10-04 01:34:41 +0300

    extra challenge w4

---
+++ Week4/XtrChallenge_W4/XtrChallengeW4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int GenerateRandomNumber();
+
+int main()
+{
+    srand((unsigned)time(0)); // Randomizing the seed using current time
+    cout << "Lets generate a random number." << endl;
+    GenerateRandomNumber(); // calling the generate function
+}
+
+int GenerateRandomNumber()
+{
+    int randomNumber = 1 + (rand() % 98); //generate random number between 0 and 99
+
+    cout << "Press the ENTER key to generate the number ";
+    if (cin.get() == '\n') // Checks if the user presses ENTER
+    {
+        cout << "Good job.\n";
+        if (randomNumber < 50)
+        {
+            cout << "The rundom number is : " << randomNumber << " which is less than 50" << endl;
+        }
+        else
+        {
+            cout << "The number is : " << randomNumber << " which is " << randomNumber - 50 << " more than 50" << endl;
+        }
+    }
+    else
+    {
+        cout << "I meant ONLY the ENTER key... Oh well.\n";
+    }
+    return randomNumber;
+}
